2d190b0abaa90af916c94d3ab05f9094d67d2c8c
Supritha.h <Supritha.h@DESKTOP-FE2IBD3>
2020-05-15 11:35:03 +0530

    Calender and java alerts

--- SeleniumPractices/src/com/selenium/practice/Dropdown.java
+++ SeleniumPractices/src/com/selenium/practice/Dropdown.java
@@ -1,4 +1,7 @@
 package com.selenium.practice;
+import org.testng.Assert;
+
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,34 +16,48 @@ public class Dropdown {
 				"C:\\Users\\Supritha.h\\Eclipse workplace\\SeleniumPractices\\drivers\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://spicejet.com");
-		// Static dropdown handling
-//		Select s=new Select(driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency")));
-//	    s.selectByValue("INR");
-//	    s.selectByIndex(3);
-		
-//		driver.findElement(By.id("divpaxinfo")).click();
-//		Thread.sleep(3000);
-//			
-//		Select s=new Select(driver.findElement(By.id("ctl00_mainContent_ddl_Adult")));
-//		s.selectByValue("5");
-		
-		//Handling checkboxes
-		WebElement checkbox=driver.findElement(By.cssSelector("input[id*='friendsandfamily']"));
-		checkbox.click();
-		System.out.println(checkbox.isSelected());
-		System.out.println(driver.findElements(By.xpath("//input[@type='checkbox']")).size());
-		
 		
 		//Dynamic dropdown handling
-		Thread.sleep(2000);
+		Thread.sleep(7000);
 		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();		
 		driver.findElement(By.xpath("//a[@value='BLR']")).click();
 		Thread.sleep(3000);
 		//driver.findElement(By.xpath("(//a[@value='IXG'])[2]")).click();
 		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='IXG']")).click();
 		
+		driver.findElement(By.cssSelector(".ui-state-default.ui-state-highlight.ui-state-active")).click();
 		
+		if (driver.findElement(By.id("Div1")).getAttribute("style").contains("0.5")) {
+			System.out.println("Its disabled");
+			Assert.assertTrue(true);
+		}else {
+			Assert.assertTrue(false);
+		}
+		
+		driver.findElement(By.id("divpaxinfo")).click();
+		Thread.sleep(3000);
+			
+		Select s=new Select(driver.findElement(By.id("ctl00_mainContent_ddl_Adult")));
+		s.selectByValue("5");
+		driver.findElement(By.id("divpaxinfo")).getText();
+		//Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
+
+		
+		// Static dropdown handling
+		Select ss=new Select(driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency")));
+	    ss.selectByValue("INR");
+	    ss.selectByIndex(3);
+		
+		
+		//Handling checkboxes
+				WebElement checkbox=driver.findElement(By.cssSelector("input[id*='friendsandfamily']"));
+				checkbox.click();
+				//System.out.println(checkbox.isSelected());
+		        Assert.assertTrue(checkbox.isSelected());
+				System.out.println(driver.findElements(By.xpath("//input[@type='checkbox']")).size());
+				
+				driver.findElement(By.name("ctl00$mainContent$btn_FindFlights")).click();
 		
 	}
-
+	
 }
---
+++ SeleniumPractices/src/com/selenium/practice/JavaAlerts.java
@@ -0,0 +1,31 @@
+package com.selenium.practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaAlerts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Supritha.h\\Eclipse workplace\\SeleniumPractices\\drivers\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.findElement(By.id("name")).sendKeys("Rahul");
+		driver.findElement(By.id("alertbtn")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+		driver.findElement(By.id("name")).sendKeys("Rahul");
+		driver.findElement(By.id("confirmbtn")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().dismiss();
+		driver.close();
+
+		
+
+		
+		
+	}
+
+}
